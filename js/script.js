098dab5d26ad07cf341ed31d37f2b82098087985
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-07-21 18:36:25 +0530

    adds a separate fucntion to create a tweet item

--- js/script.js
+++ js/script.js
@@ -5,13 +5,20 @@ tweetForm.addEventListener("submit", function (e) {
   e.preventDefault();
   console.log("submitted");
 
-  let username = document.querySelector("form").elements.username.value;
-  let tweet = document.querySelector("form").elements.tweet.value;
+  let username = document.querySelector("form").elements.username;
+  let tweet = document.querySelector("form").elements.tweet;
 
+  createTweetListItem(username.value, tweet.value);
+  username.value = "";
+  tweet.value = "";
+});
+
+createTweetListItem = (username, tweet) => {
   let tweetItem = document.createElement("li");
   tweetItem.classList.add("list-group-item");
   tweetItem.classList.add("tweet-list-item");
   tweetItem.innerText = `${username} --> ${tweet}`;
 
   tweetList.append(tweetItem);
-});
+  return tweetItem;
+};
